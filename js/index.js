a1110b1eb23c7a3b9e5421133f0ddd7616c684aa
team 502 <tearstopromise@gmail.com>
2015-01-17 12:13:45 -0800

    asdas
    asdasdd

--- js/index.js
+++ js/index.js
@@ -78,6 +78,7 @@ document.getElementById("picc1").src = "data:image/jpeg;base64," +  imageData;
         }); */
 		  navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
             destinationType: Camera.DestinationType.FILE_URI,
+				saveToPhotoAlbum: true,
 			targetWidth : 500,
 			targetHeight : 500 
         });
